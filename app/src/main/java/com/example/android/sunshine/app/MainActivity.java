3394c7fdafcacec448fde8f4b81c2de3be418def
Vitaliy Gryukov <vitgee02@gmail.com>
2015-04-09 14:27:55 +0300

    fs

---
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.android.sunshine.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private ArrayAdapter<String> mForecastAdapter;
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            String[] forecastArray = {
+                    "Сегодня        Солнечно        +17",
+                    "Завтра         Облачно         +12",
+                    "Среда          Облачно         +14",
+                    "Четверг        Дождь           +8",
+                    "Пятница        Туман           +10",
+                    "Суббота        Солнечно        +15",
+                    "Вскрес         Солнечно        +18",
+                    "Пнд            Туман           +10",
+                    "Вт             Облачно         +12",
+                    "Срд            Солнечно        +19",
+                    "Чтв            Облачно         +14",
+                    "Птн            Солнечно        +20",
+                    "Субб           Солнечно        +22",
+                    "Вскр           Облачно         +19",
+                    "Пнд            Облачно         +18",
+                    "Вт             Дождь           +14",
+                    "Срд            Туман           +16",
+                    "Чтв            Солнечно        +22",
+                    "Птн            Солнечно        +23",
+                    "Субб           Солнечно        +23",
+                    "Вскр            Облачно        +20"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(
+                    Arrays.asList(forecastArray));
+
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast
+            );
+            ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+            return rootView;
+        }
+    }
+}
